ad3a465d2caa60926acd8bef40e494738ac223e3
Bianca Teixeira <Bianca Teixeira@BIANCA>
2025-12-01 21:31:36 -0300

    Add project files.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+void ExibirMensagem()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+    Console.WriteLine("Boas vindas ao Screen Sound!");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+    Console.Write("\nDigite a sua opção: ");
+    int opcao = Convert.ToInt32(Console.ReadLine()!);
+    
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case -1:
+            break;
+        default: 
+            Console.WriteLine("Opção inválida"); 
+            break;
+    }
+}
+
+ExibirMensagem();
+ExibirOpcoesDoMenu();
